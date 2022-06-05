aa238deb927513c071da0e71060deeecd0d65725
GBell94 <gmb1294@gmail.com>
2022-06-05 10:10:06 +0000

    Add categories and add-categories html files

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -2,6 +2,18 @@ from flask import render_template
 from taskmanager import app, db
 from taskmanager.models import Category, Task
 
+
 @app.route("/")
 def home():
     return render_template("tasks.html")
+
+
+@app.route("/categories")
+def categories():
+    return render_template("categories.html")
+
+
+@app.route("/add_category", methods = ["GET", "POST"])
+    return render_template("add_category.html")
+
+
